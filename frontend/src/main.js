399942e1fc6b92bc556b78094176e7ab35b742b7
kiruxda <qkly1337@mail.ru>
2024-11-29 15:47:31 +0300

    add folder

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 const app = createApp(App)
 
